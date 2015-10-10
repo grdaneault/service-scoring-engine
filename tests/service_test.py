fad9d8116414a7cf9d39acc76d6866758ab49c72
Greg Daneault <gregdaneault@gmail.com>
2015-10-10 10:15:12 -0400

    Update service test to use common table creation

--- tests/service_test.py
+++ tests/service_test.py
@@ -3,25 +3,7 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.orm.session import Session
 
 # Import all persistence classes so that the proper tables get built for testing
-# noinspection PyUnresolvedReferences
-from checks.services import CheckCredentials
-# noinspection PyUnresolvedReferences
-from checks.services.check_dns import DnsService, DnsCheck
-# noinspection PyUnresolvedReferences
-from checks.services.check_ftp import FtpService, FtpCheck
-# noinspection PyUnresolvedReferences
-from checks.services.check_ping import PingService, PingCheck
-# noinspection PyUnresolvedReferences
-from checks.services.check_mysql import MysqlService, MysqlCheck
-# noinspection PyUnresolvedReferences
-from checks.services.check_ssh import SshService, SshCheck
-# noinspection PyUnresolvedReferences
-from checks.services.check_web import WebService, WebCheck
-# noinspection PyUnresolvedReferences
-from teams.team import Team
-
-from configuration.persistence import Base, engine
-
+from configuration.models import create_tables
 
 def setup_module():
     global transaction, connection, engine
@@ -30,7 +12,7 @@ def setup_module():
     engine = create_engine('mysql+mysqlconnector://greg:greg@192.168.243.100/scoring_engine_test')
     connection = engine.connect()
     transaction = connection.begin()
-    Base.metadata.create_all(connection)
+    create_tables(connection)
 
 
 def teardown_module():
